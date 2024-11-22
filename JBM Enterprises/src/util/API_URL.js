fac5758d8c13d568a70bdef659c0dd8669bbe248
mustafa42004 <mustafa.aerozefcreations@gmail.com>
2024-11-22 18:18:59 +0530

    api url changed

--- JBM Enterprises/src/util/API_URL.js
+++ JBM Enterprises/src/util/API_URL.js
@@ -1,6 +1,6 @@
 // const API_URL = "https://jmb-server.onrender.com/admin" render
 // const API_URL = "http://localhost:8080/admin" local
 // const API_URL = "https://dolphin-app-i8qlu.ondigitalocean.app/admin" digiocean
-const API_URL = "http://44.243.42.172:8080/admin"
+const API_URL = "https://api.jmb-enterprises.in/admin"
 
 export {API_URL};
